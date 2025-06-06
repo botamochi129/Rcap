ae6cdf055b650058a6d522b15d6d5f2944bf3dfe
botamochi129 <botamochi.129.mtr@gmail.com>
2025-06-06 13:30:52 +0900

    test

--- src/client/java/com/botamochi/rcap/client/PassengerClientCache.java
+++
@@ -1,9 +0,0 @@
-package com.botamochi.rcap.client;
-
-import com.botamochi.rcap.data.PassengerRenderData;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-public class PassengerClientCache {
-    public static final List<PassengerRenderData> PASSENGERS = new CopyOnWriteArrayList<>();
-}
